6bf388bb954648b7e7701539028de33cb8b75be0
TinchooBielenia <martinnike32@hotmail.com>
2025-07-18 02:21:29 -0300

    FinalZone update

--- Assets/Tincho/Gameplay/Scripts/Objetos/LastVictoryCage/CageMetallicDoor.cs
+++ Assets/Tincho/Gameplay/Scripts/Objetos/LastVictoryCage/CageMetallicDoor.cs
@@ -6,23 +6,23 @@ public class CageMetallicDoor : MonoBehaviour
     private Animator _metallicDoorAnimation;
     private bool _puzzleWasSolved;
     [SerializeField] private AudioSource _openedDoorSFX;
-    [SerializeField] private EnemySpawner _enemySpawner1;
-    [SerializeField] private EnemySpawner _enemySpawner2;
-    [SerializeField] private int _enemyQuantity;
+    //[SerializeField] private EnemySpawner _enemySpawner1;
+    //[SerializeField] private EnemySpawner _enemySpawner2;
+    //[SerializeField] private int _enemyQuantity;
     [SerializeField] private Animator _mainGate;
     [SerializeField] private GameObject _blockerDoor;
     [SerializeField] private float _openMainDoorTimer;
-    [SerializeField] private bool _playerCrossedTrigger;
+    //[SerializeField] private bool _playerCrossedTrigger;
     [SerializeField] private bool _timerFinished;
-    [SerializeField] private TextMeshProUGUI _timerOnScreenText;
-    [SerializeField] private GameObject _timerOnScreenCanvas;
+    //[SerializeField] private TextMeshProUGUI _timerOnScreenText;
+    //[SerializeField] private GameObject _timerOnScreenCanvas;
 
     private void Start()
     {
         _metallicDoorAnimation = GetComponent<Animator>();
         _blockerDoor.SetActive(false);
-        _playerCrossedTrigger = false;
-        _timerOnScreenCanvas.SetActive(false);
+        //_playerCrossedTrigger = false;
+        //_timerOnScreenCanvas.SetActive(false);
         _timerFinished = false;
     }
 
@@ -38,10 +38,10 @@ public class CageMetallicDoor : MonoBehaviour
             OpenMetallicDoor();
         }
 
-        if (_playerCrossedTrigger)
-        {
-            ManageBattleTimer();
-        }
+        //if (_playerCrossedTrigger)
+        //{
+        //    ManageBattleTimer();
+        //}
 
         if (_timerFinished)
         {
@@ -60,29 +60,29 @@ public class CageMetallicDoor : MonoBehaviour
     {
         Debug.Log("Player entro");
         _blockerDoor.SetActive(true);
-        _enemySpawner1.StartSpawning(_enemyQuantity);
-        _enemySpawner2.StartSpawning(_enemyQuantity);
+        //_enemySpawner1.StartSpawning(_enemyQuantity);
+        //_enemySpawner2.StartSpawning(_enemyQuantity);
         _openedDoorSFX.Play();
         _metallicDoorAnimation.SetTrigger("doorClosed");
         _mainGate.SetTrigger("closeMainGate");
-        _playerCrossedTrigger = true;
+        //_playerCrossedTrigger = true;
     }
 
-    private void ManageBattleTimer()
-    {
-        _timerOnScreenCanvas.SetActive(true);
-        _openMainDoorTimer -= Time.deltaTime;
-        _timerOnScreenText.SetText(((int)_openMainDoorTimer).ToString());
+    //private void ManageBattleTimer()
+    //{
+    //    _timerOnScreenCanvas.SetActive(true);
+    //    _openMainDoorTimer -= Time.deltaTime;
+    //    _timerOnScreenText.SetText(((int)_openMainDoorTimer).ToString());
 
-        if (_openMainDoorTimer <= 0)
-        {
-            _timerFinished = true;
-        }
-    }
+    //    if (_openMainDoorTimer <= 0)
+    //    {
+    //        _timerFinished = true;
+    //    }
+    //}
 
     private void TimerFinished()
     {
-        _timerOnScreenCanvas.SetActive(false);
+        //_timerOnScreenCanvas.SetActive(false);
         Debug.Log("Player logro escapar");
         _mainGate.SetTrigger("mainGateOpened");
     }
